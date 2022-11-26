b24e0f2d1a1b1a7e33650381eadbaa5173a1afb2
Matheus Nunes <105248264+MatheusNuness@users.noreply.github.com>
2022-11-25 23:19:17 -0300

    Add files via upload

---
+++ tecnologia.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+lista_noticias = []
+
+response = requests.get('https://g1.globo.com/tecnologia/')
+content = response.content
+site = BeautifulSoup(content, 'html.parser')
+
+noticias = site.findAll('div', attrs={'class': 'feed-post-body'})
+
+for noticia in noticias:
+
+  titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
+
+ # print(titulo.text)
+  # print(titulo['href']) # link da notícia
+
+  subtitulo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+
+  if (subtitulo):
+    # print(subtitulo.text)
+    lista_noticias.append([titulo.text, subtitulo.text, titulo['href']])
+  else:
+    lista_noticias.append([titulo.text, '', titulo['href']])
+
+news = pd.DataFrame(lista_noticias, columns=['Título', 'Subtítulo', 'Link'])
+news.to_excel('noticias_Tecnologia.xlsx', index=False)
+
+print(news)
